52004fed28ed88079beb049b5c1cc3f6ccf908a2
ToanLa <lqtoan20@gmail.com>
2023-04-28 10:33:15 +0700

    update code

---
+++ Task5/Exercise_1/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_instance" "Udacity-T2" {
+    ami = "ami-0742b4e673072066f"
+    instance_type = "t2.micro"
+    count = 4
+    tags = {
+        "name" = "Udacity Terraform"
+    }
+}
+
+resource "aws_instance" "Udacity-M4" {
+    ami = "ami-0742b4e673072066f"
+    instance_type = "m4.large"
+    count = 2
+    tags = {
+        "name" = "Udacity Terraform"
+    }
+}   
